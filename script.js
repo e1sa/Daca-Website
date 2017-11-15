d5493d24685e6fee22c29d17a9b3b2015cd0335a
e1sa <32754176+e1sa@users.noreply.github.com>
2017-11-14 16:34:18 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function family(){
+     document.getElementById("category").innerHTML = " family story";
+
+}
+function school(){
+	document.getElementById("time").innerHTML = " student story ";
+}
