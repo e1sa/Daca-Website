f8a8bc936f6290032c3f4ca10a29b5f3be159f64
e1sa <32754176+e1sa@users.noreply.github.com>
2017-11-29 18:08:13 -0800

    Add files via upload

--- bubble.js
+++ bubble.js
@@ -1,61 +1,61 @@
 
 var top10Data = [{
     "Country": "Mexico\n548k",
-    "Color": "#", 
+    "Color": "#586994", 
     "Number": "548000",
     "Percent": "79.4"
   },
   {
     "Country": "El Salvador\n26k",
-    "Color": "#EE0000",
+    "Color": "#7D869C",
     "Number": "25900",
     "Percent": "3.7"
   },
   {
     "Country": "Guatemala\n17k",
-    "Color": "#DD0000",
+    "Color": "#A2ABAB",
     "Number": "17700",
     "Percent": "2.6"
   },
   {
     "Country": "Honduras\n16k",
-    "Color": "#CC0000",
+    "Color": "#E5E8B6",
     "Number": "16100",
     "Percent": "2.3"
   },
   {
     "Country": "Peru\n7.4k",
-    "Color": "#BB0000",
+    "Color": "#B4C4AE",
     "Number": "7420",
     "Percent": "1.1"
   },
   {
     "Country": "South Korea\n7.3k",
-    "Color": "#AA0000",
+    "Color": "#28464B",
     "Number": "7310",
     "Percent": "1.1"
   },
   {
     "Country": "Brazil\n6k",
-    "Color": "#990000",
+    "Color": "#90A955",
     "Number": "5780",
     "Percent": "0.8"
   },
   {
     "Country": "Ecuador\n5.5k",
-    "Color": "#880000",
+    "Color": "#AAADC4",
     "Number": "5460",
     "Percent": "0.8"
   },
   {
     "Country": "Colombia\n5k",
-    "Color": "#770000",
+    "Color": "#68534D",
     "Number": "5020",
     "Percent": "0.7"
   },
   {
     "Country": "Argentina\n4k",
-    "Color": "#660000",
+    "Color": "#CE8D66",
     "Number": "3970",
     "Percent": "0.6"
   }, {
@@ -87,55 +87,76 @@ function draw() {
   scale(scaleValue);
 
   push();
-  translate(canvasMax / 2, 0);
-  background('#D6F5D6');
+  translate(canvasMax / 2, canvasMax / 2);
+  background('#FFF');
 
   textAlign(CENTER, CENTER);
-  fill(top10Data[0].Color);
-  ellipse(0, 200, Math.log10(top10Data[0].Number) * graphScale, Math.log10(top10Data[0].Number) * graphScale);
-  fill('#000000');
-  text(top10Data[0].Country, 0, 200)
 
-  fill(top10Data[1].Color);
-  ellipse(0, 310, Math.log10(top10Data[1].Number) * graphScale, Math.log10(top10Data[1].Number) * graphScale);
-  fill('#000000');
-  text(top10Data[1].Country, 0,310)
+    fill(top10Data[0].Color);
+  var counter = 0;
+  var radius = 150;
+  while(counter < top10Data.length){
+    push();
+      if(counter <=0){
+        translate(0, 0);
+      }else{
+        translate(radius*cos(radians(counter*36)), radius*sin(radians(counter*36)));
+      }
+      fill(top10Data[counter].Color);
+      ellipse(0, 0, Math.log10(top10Data[counter].Number) * graphScale, Math.log10(top10Data[counter].Number) * graphScale);
+      fill('#000000');
+      text(top10Data[counter].Country, 0, 0);
+    counter = counter + 1;
+    pop()
+  }
 
-  fill(top10Data[2].Color);
-  ellipse(0, 400, Math.log10(top10Data[2].Number) * graphScale, Math.log10(top10Data[2].Number) * graphScale);
-  fill('#000000');
-  text(top10Data[2].Country, 0, 400)
+//   fill(top10Data[0].Color);
+//   ellipse(0, 200, Math.log10(top10Data[0].Number) * graphScale, Math.log10(top10Data[0].Number) * graphScale);
+//   fill('#000000');
+//   text(top10Data[0].Country, 0, 200)
 
-  fill(top10Data[3].Color);
-  ellipse(100, 400, Math.log10(top10Data[3].Number) * graphScale, Math.log10(top10Data[3].Number) * graphScale);
-  fill('#000000');
-  text(top10Data[3].Country, 100, 400)
+//   fill(top10Data[1].Color);
+//   ellipse(-100, 310, Math.log10(top10Data[1].Number) * graphScale, Math.log10(top10Data[1].Number) * graphScale);
+//   fill('#000000');
+//   text(top10Data[1].Country, -100,310)
 
-  fill(top10Data[4].Color);
-  ellipse(100, 100, Math.log10(top10Data[4].Number) * graphScale, Math.log10(top10Data[4].Number) * graphScale);
-  fill('#000000');
-  text(top10Data[4].Country, 100, 100)
+//   fill(top10Data[2].Color);
+//   ellipse(-160, 210, Math.log10(top10Data[2].Number) * graphScale, Math.log10(top10Data[2].Number) * graphScale);
+//   fill('#000000');
+//   text(top10Data[2].Country, -160, 210)
 
-fill(top10Data[5].Color);
-  ellipse(100, 300, Math.log10(top10Data[5].Number) * graphScale, Math.log10(top10Data[5].Number) * graphScale);
-  fill('#000000');
-  text(top10Data[5].Country, 100, 300)
+//   fill(top10Data[3].Color);
+//   ellipse(-60, 400, Math.log10(top10Data[3].Number) * graphScale, Math.log10(top10Data[3].Number) * graphScale);
+//   fill('#000000');
+//   text(top10Data[3].Country,-60, 400)
 
-fill(top10Data[6].Color);
-  ellipse(100, 210, Math.log10(top10Data[6].Number) * graphScale, Math.log10(top10Data[6].Number) * graphScale);
-  fill('#000000');
-  text(top10Data[6].Country, 100, 210)
+//   fill(top10Data[4].Color);
+//   ellipse(-60, 100, Math.log10(top10Data[4].Number) * graphScale, Math.log10(top10Data[4].Number) * graphScale);
+//   fill('#000000');
+//   text(top10Data[4].Country, -60, 100)
 
-  fill(top10Data[7].Color);
-  ellipse(100, 300, Math.log10(top10Data[7].Number) * graphScale, Math.log10(top10Data[7].Number) * graphScale);
-  fill('#000000');
-  text(top10Data[7].Country, 100, 300)
+// fill(top10Data[5].Color);
+//   ellipse(-150, 150, Math.log10(top10Data[5].Number) * graphScale, Math.log10(top10Data[5].Number) * graphScale);
+//   fill('#000000');
+//   text(top10Data[5].Country, -150,150)
 
-  fill(top10Data[8].Color);
-  ellipse(100, 200, Math.log10(top10Data[8].Number) * graphScale, Math.log10(top10Data[8].Number) * graphScale);
-  fill('#000000');
-  text(top10Data[8].Country, 100, 200)
+// fill(top10Data[6].Color);
+//   ellipse(100, 290, Math.log10(top10Data[6].Number) * graphScale, Math.log10(top10Data[6].Number) * graphScale);
+//   fill('#000000');
+//   text(top10Data[6].Country, 100, 290)
+
+//   fill(top10Data[7].Color);
+//   ellipse(-100, 225, Math.log10(top10Data[7].Number) * graphScale, Math.log10(top10Data[7].Number) * graphScale);
+//   fill('#000000');
+//   text(top10Data[7].Country, -100,225)
+
+//   fill(top10Data[8].Color);
+//   ellipse(100, 250, Math.log10(top10Data[8].Number) * graphScale, Math.log10(top10Data[8].Number) * graphScale);
+//   fill('#000000');
+//   text(top10Data[8].Country, 100, 250)
+  
   pop();
+
 }
 
 
--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 function family(){
      document.getElementById("category").innerHTML = "I Just Got DACA, And Now Trump Is Canceling It";
-     document.getElementById("author").innerHTML = "Juan Mireles";
-     document.getElementById("pic").src = "https://youthradio.org/wp-content/uploads/2017/09/IMG_5154.jpg";
+     document.getElementById("author").innerHTML = "Juan Mireles ";
+ 	 document.getElementById("pic").src = "https://youthradio.org/wp-content/uploads/2017/09/IMG_5154.jpg";
+   document.getElementById("sound").src = "https://youthradio.org/wp-content/uploads/2017/09/Juan-Mireles-2.mp4";
+   document.getElementById("stories").style.display = "block";
 }
 
 function school(){
-	 document.getElementById("category").innerHTML = "Undocumented UC Berkeley Student Refuses to Live in Fear ";
-     document.getElementById("author").innerHTML = "Juan Prieto";
-     document.getElementById("pic").src = "https://s3.amazonaws.com/media.youthradio.org/wp-content/uploads/2017/02/08134758/Juan-Prieto.jpg";
-
+	document.getElementById("category").innerHTML="Undocumented UC Berkeley Student Refuses to Live in Fear ";
+  document.getElementById("author").src = "Juan Prieto ";
+  document.getElementById("pic").src = "https://s3.amazonaws.com/media.youthradio.org/wp-content/uploads/2017/02/08134758/Juan-Prieto.jpg";
+  document.getElementById("stories").style.display = "block";
 }
 
+
 function work(){
-		 document.getElementById("category").innerHTML = "Piecemeal Immigration Reform Leaves Student Better Off For Now";
-         document.getElementById("author").innerHTML = "Luz Elena Hernandez";
-         document.getElementById("pic").src = "https://s3.amazonaws.com/media.youthradio.org/wp-content/uploads/2014/03/LuzElanaHernandez.jpg";
-         document.getElementById("sound").src = "https://s3.amazonaws.com/media.youthradio.org/wp-content/uploads/2014/01/YR_Luz_Social-Number_KCBS.mp3";
+	document.getElementById("category").innerHTML=" Piecemeal Immigration Reform Leaves Student Better Off For Now";
+  document.getElementById("author").innerHTML = "Luz Elena Hernandez ";
+  document.getElementById("pic").src = "https://s3.amazonaws.com/media.youthradio.org/wp-content/uploads/2014/03/LuzElanaHernandez.jpg";
+  document.getElementById("sound").src = "https://s3.amazonaws.com/media.youthradio.org/wp-content/uploads/2014/01/YR_Luz_Social-Number_KCBS.mp3"
+    document.getElementById("stories").style.display = "block";
 
      }
 
 
-     
+
+	
